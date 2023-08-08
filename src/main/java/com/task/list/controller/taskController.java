996a94334c1b88f5cb3a666db4833df0f2d4f0bf
GabsLopes055 <lopesgabriel055@gmail.com>
2023-08-07 21:04:05 -0300

    Criado a camada de deletar uma tarefa

--- src/main/java/com/task/list/controller/taskController.java
+++ src/main/java/com/task/list/controller/taskController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.annotation.Repeatable;
 import java.util.List;
 
 @RestController
@@ -45,6 +46,17 @@ public class taskController {
 
         return ResponseEntity.status(HttpStatus.OK).body(service.updateTask(request, cdTask));
 
+    }
+
+    /*/
+    metodo para excluir uma tarefa já concluida
+     */
+    public ResponseEntity<?> deleteTask(@PathVariable(value = "cdTask") Long cdTask) {
+        if(service.deleteTask(cdTask)) {
+            return ResponseEntity.status(HttpStatus.OK).body("Tarefa Deletada");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Não foi possível excluir tarefa");
+        }
     }
 
 
